bf6ccedf391350265a0a693b69b170cfc702863b
Yalifuddin <yalif1710@gmail.com>
2026-02-24 06:38:32 +0700

    add eshopapplicationtest

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -3,11 +3,20 @@ package id.ac.ui.cs.advprog.eshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
-class EshopApplicationTests {
+class EshopApplicationTest {
 
     @Test
     void contextLoads() {
+        // Verifies that the Spring context loads successfully
     }
 
+    @Test
+    void mainMethodShouldStartApplication() {
+        assertDoesNotThrow(() -> {
+            EshopApplication.main(new String[]{});
+        });
+    }
 }
