378e2b8aff4b359cf52131295e6ea40926139203
Yalifuddin <yalif1710@gmail.com>
2026-02-24 06:52:37 +0700

    fix PMD: add provate constructur and final args parameter

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EshopApplication {
 
-    public static void main(String[] args) {
+    private EshopApplication() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static void main(final String[] args) {
         SpringApplication.run(EshopApplication.class, args);
     }
 
