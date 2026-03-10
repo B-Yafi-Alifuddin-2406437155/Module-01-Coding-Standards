c615a74d5343f2956148a278a6e8673e8026d7f6
Yalifuddin <yalif1710@gmail.com>
2026-03-10 18:44:40 +0700

    [GREEN] Implement Payment model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class Payment {
+    public static final String STATUS_PENDING = "PENDING";
+    public static final String STATUS_SUCCESS = "SUCCESS";
+    public static final String STATUS_REJECTED = "REJECTED";
+
+    private final String id;
+    private final Order order;
+    private final String method;
+    private final Map<String, String> paymentData;
+    private String status;
+
+    public Payment(String id, Order order, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData == null ? new HashMap<>() : new HashMap<>(paymentData);
+        this.status = STATUS_PENDING;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
